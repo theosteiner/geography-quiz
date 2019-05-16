5caccfaa7f9e56cde261129b59a8af297d170de0
theosteiner <39752764+theosteiner@users.noreply.github.com>
2019-05-15 20:44:25 -0400

    Update AmericasQuiz.java

--- src/Americas/AmericasQuiz.java
+++ src/Americas/AmericasQuiz.java
@@ -10,7 +10,7 @@ public class AmericasQuiz extends Quiz
 	public AmericasQuiz()
 	{
 		super();
-		final File f = new File("bin/am/americasmap.png");
+		final File f = new File("images/am/americasmap.png");
 		quizDialog.addWindowListener(new WindowAdapter()
 		{
 			@Override
@@ -37,143 +37,143 @@ public class AmericasQuiz extends Quiz
 			switch (pickRandomCountry().getName())
 			{
 				case "Antigua and Barbuda":
-					file = new File("bin/am/antiguaandbarbuda.png");
+					file = new File("images/am/antiguaandbarbuda.png");
 					gui.setImage(file);
 					break;
 				case "Argentina":
-					file = new File("bin/am/argentina.png");
+					file = new File("images/am/argentina.png");
 					gui.setImage(file);
 					break;
 				case "Bahamas":
-					file = new File("bin/am/bahamas.png");
+					file = new File("images/am/bahamas.png");
 					gui.setImage(file);
 					break;
 				case "Barbados":
-					file = new File("bin/am/barbados.png");
+					file = new File("images/am/barbados.png");
 					gui.setImage(file);
 					break;
 				case "Belize":
-					file = new File("bin/am/belize.png");
+					file = new File("images/am/belize.png");
 					gui.setImage(file);
 					break;
 				case "Bolivia":
-					file = new File("bin/am/bolivia.png");
+					file = new File("images/am/bolivia.png");
 					gui.setImage(file);
 					break;
 				case "Brazil":
-					file = new File("bin/am/brazil.png");
+					file = new File("images/am/brazil.png");
 					gui.setImage(file);
 					break;
 				case "Canada":
-					file = new File("bin/am/canada.png");
+					file = new File("images/am/canada.png");
 					gui.setImage(file);
 					break;
 				case "Chile":
-					file = new File("bin/am/chile.png");
+					file = new File("images/am/chile.png");
 					gui.setImage(file);
 					break;
 				case "Colombia":
-					file = new File("bin/am/colombia.png");
+					file = new File("images/am/colombia.png");
 					gui.setImage(file);
 					break;
 				case "Costa Rica":
-					file = new File("bin/am/costarica.png");
+					file = new File("images/am/costarica.png");
 					gui.setImage(file);
 					break;
 				case "Cuba":
-					file = new File("bin/am/cuba.png");
+					file = new File("images/am/cuba.png");
 					gui.setImage(file);
 					break;
 				case "Dominica":
-					file = new File("bin/am/dominica.png");
+					file = new File("images/am/dominica.png");
 					gui.setImage(file);
 					break;
 				case "Dominican Republic":
-					file = new File("bin/am/dominicanrepublic.png");
+					file = new File("images/am/dominicanrepublic.png");
 					gui.setImage(file);
 					break;
 				case "Ecuador":
-					file = new File("bin/am/ecuador.png");
+					file = new File("images/am/ecuador.png");
 					gui.setImage(file);
 					break;
 				case "El Salvador":
-					file = new File("bin/am/elsalvador.png");
+					file = new File("images/am/elsalvador.png");
 					gui.setImage(file);
 					break;
 				case "Grenada":
-					file = new File("bin/am/grenada.png");
+					file = new File("images/am/grenada.png");
 					gui.setImage(file);
 					break;
 				case "Guatemala":
-					file = new File("bin/am/guatemala.png");
+					file = new File("images/am/guatemala.png");
 					gui.setImage(file);
 					break;
 				case "Guyana":
-					file = new File("bin/am/guyana.png");
+					file = new File("images/am/guyana.png");
 					gui.setImage(file);
 					break;
 				case "Haiti":
-					file = new File("bin/am/haiti.png");
+					file = new File("images/am/haiti.png");
 					gui.setImage(file);
 					break;
 				case "Honduras":
-					file = new File("bin/am/honduras.png");
+					file = new File("images/am/honduras.png");
 					gui.setImage(file);
 					break;
 				case "Jamaica":
-					file = new File("bin/am/jamaica.png");
+					file = new File("images/am/jamaica.png");
 					gui.setImage(file);
 					break;
 				case "Mexico":
-					file = new File("bin/am/mexico.png");
+					file = new File("images/am/mexico.png");
 					gui.setImage(file);
 					break;
 				case "Nicaragua":
-					file = new File("bin/am/nicaragua.png");
+					file = new File("images/am/nicaragua.png");
 					gui.setImage(file);
 					break;
 				case "Panama":
-					file = new File("bin/am/panama.png");
+					file = new File("images/am/panama.png");
 					gui.setImage(file);
 					break;
 				case "Paraguay":
-					file = new File("bin/am/paraguay.png");
+					file = new File("images/am/paraguay.png");
 					gui.setImage(file);
 					break;
 				case "Peru":
-					file = new File("bin/am/peru.png");
+					file = new File("images/am/peru.png");
 					gui.setImage(file);
 					break;
 				case "Saint Kitts and Nevis":
-					file = new File("bin/am/stkittsandnevis.png");
+					file = new File("images/am/stkittsandnevis.png");
 					gui.setImage(file);
 					break;
 				case "Saint Lucia":
-					file = new File("bin/am/stlucia.png");
+					file = new File("images/am/stlucia.png");
 					gui.setImage(file);
 					break;
 				case "Saint Vincent and the Grenadines":
-					file = new File("bin/am/stvincentandthegrenadines.png");
+					file = new File("images/am/stvincentandthegrenadines.png");
 					gui.setImage(file);
 					break;
 				case "Suriname":
-					file = new File("bin/am/suriname.png");
+					file = new File("images/am/suriname.png");
 					gui.setImage(file);
 					break;
 				case "Trinidad and Tobago":
-					file = new File("bin/am/trinidadandtobago.png");
+					file = new File("images/am/trinidadandtobago.png");
 					gui.setImage(file);
 					break;
 				case "United States":
-					file = new File("bin/am/unitedstates.png");
+					file = new File("images/am/unitedstates.png");
 					gui.setImage(file);
 					break;
 				case "Uruguay":
-					file = new File("bin/am/uruguay.png");
+					file = new File("images/am/uruguay.png");
 					gui.setImage(file);
 					break;
 				case "Venezuela":
-					file = new File("bin/am/venezuela.png");
+					file = new File("images/am/venezuela.png");
 					gui.setImage(file);
 					break;
 			}
