add23644eec6a88319ff2d4540869ebdf45308d7
theosteiner <39752764+theosteiner@users.noreply.github.com>
2019-05-15 20:43:19 -0400

    Update AfricaQuiz.java

--- src/Africa/AfricaQuiz.java
+++ src/Africa/AfricaQuiz.java
@@ -10,7 +10,7 @@ public class AfricaQuiz extends Quiz
 	public AfricaQuiz()
 	{
 		super();
-		final File f = new File("bin/af/africamap.png");
+		final File f = new File("images/af/africamap.png");
 		quizDialog.addWindowListener(new WindowAdapter()
 		{
 			@Override
@@ -37,219 +37,219 @@ public class AfricaQuiz extends Quiz
 			switch (pickRandomCountry().getName())
 			{
 				case "Algeria":
-					file = new File("bin/af/algeria.png");
+					file = new File("images/af/algeria.png");
 					gui.setImage(file);
 					break;
 				case "Angola":
-					file = new File("bin/af/angola.png");
+					file = new File("images/af/angola.png");
 					gui.setImage(file);
 					break;
 				case "Benin":
-					file = new File("bin/af/benin.png");
+					file = new File("images/af/benin.png");
 					gui.setImage(file);
 					break;
 				case "Botswana":
-					file = new File("bin/af/botswana.png");
+					file = new File("images/af/botswana.png");
 					gui.setImage(file);
 					break;
 				case "Burkina Faso":
-					file = new File("bin/af/burkinafaso.png");
+					file = new File("images/af/burkinafaso.png");
 					gui.setImage(file);
 					break;
 				case "Burundi":
-					file = new File("bin/af/burundi.png");
+					file = new File("images/af/burundi.png");
 					gui.setImage(file);
 					break;
 				case "Cameroon":
-					file = new File("bin/af/cameroon.png");
+					file = new File("images/af/cameroon.png");
 					gui.setImage(file);
 					break;
 				case "Cape Verde":
-					file = new File("bin/af/capeverde.png");
+					file = new File("images/af/capeverde.png");
 					gui.setImage(file);
 					break;
 				case "Central African Republic":
-					file = new File("bin/af/car.png");
+					file = new File("images/af/car.png");
 					gui.setImage(file);
 					break;
 				case "Chad":
-					file = new File("bin/af/chad.png");
+					file = new File("images/af/chad.png");
 					gui.setImage(file);
 					break;
 				case "Comoros":
-					file = new File("bin/af/comoros.png");
+					file = new File("images/af/comoros.png");
 					gui.setImage(file);
 					break;
 				case "Democratic Republic of the Congo":
-					file = new File("bin/af/drc.png");
+					file = new File("images/af/drc.png");
 					gui.setImage(file);
 					break;
 				case "Republic of the Congo":
-					file = new File("bin/af/congo.png");
+					file = new File("images/af/congo.png");
 					gui.setImage(file);
 					break;
 				case "Cote d'Ivoire":
-					file = new File("bin/af/ivorycoast.png");
+					file = new File("images/af/ivorycoast.png");
 					gui.setImage(file);
 					break;
 				case "Djibouti":
-					file = new File("bin/af/djibouti.png");
+					file = new File("images/af/djibouti.png");
 					gui.setImage(file);
 					break;
 				case "Egypt":
-					file = new File("bin/af/egypt.png");
+					file = new File("images/af/egypt.png");
 					gui.setImage(file);
 					break;
 				case "Equatorial Guinea":
-					file = new File("bin/af/equatorialguinea.png");
+					file = new File("images/af/equatorialguinea.png");
 					gui.setImage(file);
 					break;
 				case "Eritrea":
-					file = new File("bin/af/eritrea.png");
+					file = new File("images/af/eritrea.png");
 					gui.setImage(file);
 					break;
 				case "Ethiopia":
-					file = new File("bin/af/ethiopia.png");
+					file = new File("images/af/ethiopia.png");
 					gui.setImage(file);
 					break;
 				case "Gabon":
-					file = new File("bin/af/gabon.png");
+					file = new File("images/af/gabon.png");
 					gui.setImage(file);
 					break;
 				case "Gambia":
-					file = new File("bin/af/gambia.png");
+					file = new File("images/af/gambia.png");
 					gui.setImage(file);
 					break;
 				case "Ghana":
-					file = new File("bin/af/ghana.png");
+					file = new File("images/af/ghana.png");
 					gui.setImage(file);
 					break;
 				case "Guinea":
-					file = new File("bin/af/guinea.png");
+					file = new File("images/af/guinea.png");
 					gui.setImage(file);
 					break;
 				case "Guinea-Bissau":
-					file = new File("bin/af/guineabissau.png");
+					file = new File("images/af/guineabissau.png");
 					gui.setImage(file);
 					break;
 				case "Kenya":
-					file = new File("bin/af/kenya.png");
+					file = new File("images/af/kenya.png");
 					gui.setImage(file);
 					break;
 				case "Lesotho":
-					file = new File("bin/af/lesotho.png");
+					file = new File("images/af/lesotho.png");
 					gui.setImage(file);
 					break;
 				case "Liberia":
-					file = new File("bin/af/liberia.png");
+					file = new File("images/af/liberia.png");
 					gui.setImage(file);
 					break;
 				case "Libya":
-					file = new File("bin/af/libya.png");
+					file = new File("images/af/libya.png");
 					gui.setImage(file);
 					break;
 				case "Madagascar":
-					file = new File("bin/af/madagascar.png");
+					file = new File("images/af/madagascar.png");
 					gui.setImage(file);
 					break;
 				case "Malawi":
-					file = new File("bin/af/malawi.png");
+					file = new File("images/af/malawi.png");
 					gui.setImage(file);
 					break;
 				case "Mali":
-					file = new File("bin/af/mali.png");
+					file = new File("images/af/mali.png");
 					gui.setImage(file);
 					break;
 				case "Mauritania":
-					file = new File("bin/af/mauritania.png");
+					file = new File("images/af/mauritania.png");
 					gui.setImage(file);
 					break;
 				case "Mauritius":
-					file = new File("bin/af/mauritius.png");
+					file = new File("images/af/mauritius.png");
 					gui.setImage(file);
 					break;
 				case "Morocco":
-					file = new File("bin/af/morocco.png");
+					file = new File("images/af/morocco.png");
 					gui.setImage(file);
 					break;
 				case "Mozambique":
-					file = new File("bin/af/mozambique.png");
+					file = new File("images/af/mozambique.png");
 					gui.setImage(file);
 					break;
 				case "Namibia":
-					file = new File("bin/af/namibia.png");
+					file = new File("images/af/namibia.png");
 					gui.setImage(file);
 					break;
 				case "Niger":
-					file = new File("bin/af/niger.png");
+					file = new File("images/af/niger.png");
 					gui.setImage(file);
 					break;
 				case "Nigeria":
-					file = new File("bin/af/nigeria.png");
+					file = new File("images/af/nigeria.png");
 					gui.setImage(file);
 					break;
 				case "Rwanda":
-					file = new File("bin/af/rwanda.png");
+					file = new File("images/af/rwanda.png");
 					gui.setImage(file);
 					break;
 				case "Sao Tome and Principe":
-					file = new File("bin/af/saotomeandprincipe.png");
+					file = new File("images/af/saotomeandprincipe.png");
 					gui.setImage(file);
 					break;
 				case "Senegal":
-					file = new File("bin/af/senegal.png");
+					file = new File("images/af/senegal.png");
 					gui.setImage(file);
 					break;
 				case "Seychelles":
-					file = new File("bin/af/seychelles.png");
+					file = new File("images/af/seychelles.png");
 					gui.setImage(file);
 					break;
 				case "Sierra Leone":
-					file = new File("bin/af/sierraleone.png");
+					file = new File("images/af/sierraleone.png");
 					gui.setImage(file);
 					break;
 				case "Somalia":
-					file = new File("bin/af/somalia.png");
+					file = new File("images/af/somalia.png");
 					gui.setImage(file);
 					break;
 				case "South Africa":
-					file = new File("bin/af/southafrica.png");
+					file = new File("images/af/southafrica.png");
 					gui.setImage(file);
 					break;
 				case "South Sudan":
-					file = new File("bin/af/southsudan.png");
+					file = new File("images/af/southsudan.png");
 					gui.setImage(file);
 					break;
 				case "Sudan":
-					file = new File("bin/af/sudan.png");
+					file = new File("images/af/sudan.png");
 					gui.setImage(file);
 					break;
 				case "Swaziland":
-					file = new File("bin/af/swaziland.png");
+					file = new File("images/af/swaziland.png");
 					gui.setImage(file);
 					break;
 				case "Tanzania":
-					file = new File("bin/af/tanzania.png");
+					file = new File("images/af/tanzania.png");
 					gui.setImage(file);
 					break;
 				case "Togo":
-					file = new File("bin/af/togo.png");
+					file = new File("images/af/togo.png");
 					gui.setImage(file);
 					break;
 				case "Tunisia":
-					file = new File("bin/af/tunisia.png");
+					file = new File("images/af/tunisia.png");
 					gui.setImage(file);
 					break;
 				case "Uganda":
-					file = new File("bin/af/uganda.png");
+					file = new File("images/af/uganda.png");
 					gui.setImage(file);
 					break;
 				case "Zambia":
-					file = new File("bin/af/zambia.png");
+					file = new File("images/af/zambia.png");
 					gui.setImage(file);
 					break;
 				case "Zimbabwe":
-					file = new File("bin/af/zimbabwe.png");
+					file = new File("images/af/zimbabwe.png");
 					gui.setImage(file);
 					break;
 			}
