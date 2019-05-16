49780727c61677853cd3b25428315b2a96dac419
theosteiner <39752764+theosteiner@users.noreply.github.com>
2019-05-15 20:45:09 -0400

    Update AsiaQuiz.java

--- src/Asia/AsiaQuiz.java
+++ src/Asia/AsiaQuiz.java
@@ -10,7 +10,7 @@ public class AsiaQuiz extends Quiz
 	public AsiaQuiz()
 	{
 		super();
-		final File f = new File("bin/as/asiamap.png");
+		final File f = new File("images/as/asiamap.png");
 		quizDialog.addWindowListener(new WindowAdapter()
 		{
 			@Override
@@ -37,203 +37,203 @@ public class AsiaQuiz extends Quiz
 			switch (pickRandomCountry().getName())
 			{
 				case "Afghanistan":
-					file = new File("bin/as/afghanistan.png");
+					file = new File("images/as/afghanistan.png");
 					gui.setImage(file);
 					break;
 				case "Armenia":
-					file = new File("bin/as/armenia.png");
+					file = new File("images/as/armenia.png");
 					gui.setImage(file);
 					break;
 				case "Azerbaijan":
-					file = new File("bin/as/azerbaijan.png");
+					file = new File("images/as/azerbaijan.png");
 					gui.setImage(file);
 					break;
 				case "Bahrain":
-					file = new File("bin/as/bahrain.png");
+					file = new File("images/as/bahrain.png");
 					gui.setImage(file);
 					break;
 				case "Bangladesh":
-					file = new File("bin/as/bangladesh.png");
+					file = new File("images/as/bangladesh.png");
 					gui.setImage(file);
 					break;
 				case "Bhutan":
-					file = new File("bin/as/bhutan.png");
+					file = new File("images/as/bhutan.png");
 					gui.setImage(file);
 					break;
 				case "Brunei":
-					file = new File("bin/as/brunei.png");
+					file = new File("images/as/brunei.png");
 					gui.setImage(file);
 					break;
 				case "Cambodia":
-					file = new File("bin/as/cambodia.png");
+					file = new File("images/as/cambodia.png");
 					gui.setImage(file);
 					break;
 				case "China":
-					file = new File("bin/as/china.png");
+					file = new File("images/as/china.png");
 					gui.setImage(file);
 					break;
 				case "Cyprus":
-					file = new File("bin/as/cyprus.png");
+					file = new File("images/as/cyprus.png");
 					gui.setImage(file);
 					break;
 				case "East Timor":
-					file = new File("bin/as/easttimor.png");
+					file = new File("images/as/easttimor.png");
 					gui.setImage(file);
 					break;
 				case "Georgia":
-					file = new File("bin/as/georgia.png");
+					file = new File("images/as/georgia.png");
 					gui.setImage(file);
 					break;
 				case "India":
-					file = new File("bin/as/india.png");
+					file = new File("images/as/india.png");
 					gui.setImage(file);
 					break;
 				case "Indonesia":
-					file = new File("bin/as/indonesia.png");
+					file = new File("images/as/indonesia.png");
 					gui.setImage(file);
 					break;
 				case "Iran":
-					file = new File("bin/as/iran.png");
+					file = new File("images/as/iran.png");
 					gui.setImage(file);
 					break;
 				case "Iraq":
-					file = new File("bin/as/iraq.png");
+					file = new File("images/as/iraq.png");
 					gui.setImage(file);
 					break;
 				case "Israel":
-					file = new File("bin/as/israel.png");
+					file = new File("images/as/israel.png");
 					gui.setImage(file);
 					break;
 				case "Japan":
-					file = new File("bin/as/japan.png");
+					file = new File("images/as/japan.png");
 					gui.setImage(file);
 					break;
 				case "Jordan":
-					file = new File("bin/as/jordan.png");
+					file = new File("images/as/jordan.png");
 					gui.setImage(file);
 					break;
 				case "Kazakhstan":
-					file = new File("bin/as/kazakhstan.png");
+					file = new File("images/as/kazakhstan.png");
 					gui.setImage(file);
 					break;
 				case "Kuwait":
-					file = new File("bin/as/kuwait.png");
+					file = new File("images/as/kuwait.png");
 					gui.setImage(file);
 					break;
 				case "Kyrgyzstan":
-					file = new File("bin/as/kyrgyzstan.png");
+					file = new File("images/as/kyrgyzstan.png");
 					gui.setImage(file);
 					break;
 				case "Laos":
-					file = new File("bin/as/laos.png");
+					file = new File("images/as/laos.png");
 					gui.setImage(file);
 					break;
 				case "Lebanon":
-					file = new File("bin/as/lebanon.png");
+					file = new File("images/as/lebanon.png");
 					gui.setImage(file);
 					break;
 				case "Malaysia":
-					file = new File("bin/as/malaysia.png");
+					file = new File("images/as/malaysia.png");
 					gui.setImage(file);
 					break;
 				case "Maldives":
-					file = new File("bin/as/maldives.png");
+					file = new File("images/as/maldives.png");
 					gui.setImage(file);
 					break;
 				case "Mongolia":
-					file = new File("bin/as/mongolia.png");
+					file = new File("images/as/mongolia.png");
 					gui.setImage(file);
 					break;
 				case "Myanmar":
-					file = new File("bin/as/myanmar.png");
+					file = new File("images/as/myanmar.png");
 					gui.setImage(file);
 					break;
 				case "Nepal":
-					file = new File("bin/as/nepal.png");
+					file = new File("images/as/nepal.png");
 					gui.setImage(file);
 					break;
 				case "North Korea":
-					file = new File("bin/as/northkorea.png");
+					file = new File("images/as/northkorea.png");
 					gui.setImage(file);
 					break;
 				case "Oman":
-					file = new File("bin/as/oman.png");
+					file = new File("images/as/oman.png");
 					gui.setImage(file);
 					break;
 				case "Pakistan":
-					file = new File("bin/as/pakistan.png");
+					file = new File("images/as/pakistan.png");
 					gui.setImage(file);
 					break;
 				case "Palestine":
-					file = new File("bin/as/palestine.png");
+					file = new File("images/as/palestine.png");
 					gui.setImage(file);
 					break;
 				case "Philippines":
-					file = new File("bin/as/philippines.png");
+					file = new File("images/as/philippines.png");
 					gui.setImage(file);
 					break;
 				case "Qatar":
-					file = new File("bin/as/qatar.png");
+					file = new File("images/as/qatar.png");
 					gui.setImage(file);
 					break;
 				case "Russia":
-					file = new File("bin/as/russia.png");
+					file = new File("images/as/russia.png");
 					gui.setImage(file);
 					break;
 				case "Saudi Arabia":
-					file = new File("bin/as/saudiarabia.png");
+					file = new File("images/as/saudiarabia.png");
 					gui.setImage(file);
 					break;
 				case "Singapore":
-					file = new File("bin/as/singapore.png");
+					file = new File("images/as/singapore.png");
 					gui.setImage(file);
 					break;
 				case "South Korea":
-					file = new File("bin/as/southkorea.png");
+					file = new File("images/as/southkorea.png");
 					gui.setImage(file);
 					break;
 				case "Sri Lanka":
-					file = new File("bin/as/srilanka.png");
+					file = new File("images/as/srilanka.png");
 					gui.setImage(file);
 					break;
 				case "Syria":
-					file = new File("bin/as/syria.png");
+					file = new File("images/as/syria.png");
 					gui.setImage(file);
 					break;
 				case "Taiwan":
-					file = new File("bin/as/taiwan.png");
+					file = new File("images/as/taiwan.png");
 					gui.setImage(file);
 					break;
 				case "Tajikistan":
-					file = new File("bin/as/tajikistan.png");
+					file = new File("images/as/tajikistan.png");
 					gui.setImage(file);
 					break;
 				case "Thailand":
-					file = new File("bin/as/thailand.png");
+					file = new File("images/as/thailand.png");
 					gui.setImage(file);
 					break;
 				case "Turkey":
-					file = new File("bin/as/turkey.png");
+					file = new File("images/as/turkey.png");
 					gui.setImage(file);
 					break;
 				case "Turkmenistan":
-					file = new File("bin/as/turkmenistan.png");
+					file = new File("images/as/turkmenistan.png");
 					gui.setImage(file);
 					break;
 				case "United Arab Emirates":
-					file = new File("bin/as/uae.png");
+					file = new File("images/as/uae.png");
 					gui.setImage(file);
 					break;
 				case "Uzbekistan":
-					file = new File("bin/as/uzbekistan.png");
+					file = new File("images/as/uzbekistan.png");
 					gui.setImage(file);
 					break;
 				case "Vietnam":
-					file = new File("bin/as/vietnam.png");
+					file = new File("images/as/vietnam.png");
 					gui.setImage(file);
 					break;
 				case "Yemen":
-					file = new File("bin/as/yemen.png");
+					file = new File("images/as/yemen.png");
 					gui.setImage(file);
 					break;
 			}
